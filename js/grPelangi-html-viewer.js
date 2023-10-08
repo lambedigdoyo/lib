fc9bfa4cfc8a8e675ba420417b667bda226ed69e
Lambedigdoyo <101160626+lambedigdoyo@users.noreply.github.com>
2023-10-08 23:02:47 +0700

    Add files via upload

---
+++ js/grPelangi-html-viewer.js
@@ -0,0 +1,24 @@
+﻿//<![CDATA[
+(function () {
+  function htmlEscape(s) {
+    return s
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;');
+  }
+
+  // this page's own source code
+  var grHtml = htmlEscape(
+    '<!DOCTYPE html>\n<html>\n' +
+    document.documentElement.innerHTML.split('</body>')[0] +
+    '\n\n</body>\n<\/html>\n');
+
+  // Highlight the operative parts:
+  grHtml = grHtml.replace(
+    /&lt;script src[\s\S]*?&gt;&lt;\/script&gt;|&lt;!--\?[\s\S]*?--&gt;|&lt;pre\b[\s\S]*?&lt;\/pre&gt;/g,
+    '<span class="operative">$&<\/span>');
+
+  // insert into PRE
+  document.getElementById("gr").innerHTML = grHtml;
+})();
+//]]>
